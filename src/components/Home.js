53b9b7eaea2b127b97c895fa5f842e034ce1bc1d
Orion <weldon.orion@gmail.com>
2018-11-28 07:09:49 -0800

    FixingAllNotedIssues

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,25 +2,36 @@ import React, { Component } from 'react';
 import { Jumbotron } from 'react-bootstrap';
 
 export default class Home extends Component {
-  render() {
-    return (
-      <div className="row">
-        <div className="col-xs-1">
-        </div>
-        <Jumbotron className="home-jumbo col-xs-10">
-          <div className=" decor col-xs-2">
-            <h1>~</h1>
-          </div>
-          <div className="col-xs-8">
-            <h3 className="welcome-text">Creating beautiful cakes from my home for your unique occasion</h3>
-          </div>
-          <div className=" decor col-xs-2">
-            <h1>~</h1>
-          </div>
-        </Jumbotron>
-        <div className="col-xs-1">
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <div className="row">
+                    <div className="col-xs-4">
+                    </div>
+                    <div>
+                        <img className="landingPhoto col-xs-4" src={require("./images/landingPhoto.jpg")} alt="3 tier cake"/>
+                    </div>
+                    <div className="col-xs-4">
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-xs-1">
+                    </div>
+                    <Jumbotron className="home-jumbo col-xs-10">
+                        <div className=" decor col-xs-1">
+                            <h1>~</h1>
+                        </div>
+                        <div className="col-xs-10">
+                            <h3 className="welcome-text">Creating beautiful cakes from my home for your unique occasion</h3>
+                        </div>
+                        <div className=" decor col-xs-1">
+                            <h1>~</h1>
+                        </div>
+                    </Jumbotron>
+                    <div className="col-xs-1">
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
