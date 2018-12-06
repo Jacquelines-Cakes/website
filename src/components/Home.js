c38636f5c7c390ec863839d489c7a10f27cf050b
Pete Gomez <pete.gomez@concur.com>
2018-12-05 23:36:16 -0800

    Few minor home page tweaks

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,13 +6,13 @@ export default class Home extends Component {
         return (
             <div>
                 <div className="row">
-                    <div className="col-xs-3">
+                    <div className="col-xs-1">
                     </div>
 
                     <div className="decor col-xs-1">
                         <h1>~</h1>
                     </div>
-                    <div className="col-xs-4">
+                    <div className="col-xs-8">
                         <h3 className="welcome-text">Creating beautiful cakes from my home for your unique occasion</h3>
                     </div>
                     <div className="decor col-xs-1">
@@ -36,36 +36,3 @@ export default class Home extends Component {
         );
     }
 }
-
-
-{/*<div>*/}
-    {/*<div className="row">*/}
-        {/*<div className="col-xs-1">*/}
-
-        {/*</div>*/}
-        {/*<Jumbotron className="home-jumbo col-xs-10">*/}
-            {/*<div className="col-xs-1">*/}
-                {/*~*/}
-            {/*</div>*/}
-            {/*<div className="col-xs-10">*/}
-                {/*<h3 className="welcome-text">Creating beautiful cakes from my home for your unique occasion</h3>*/}
-            {/*</div>*/}
-
-            {/*<div className="col-xs-1">*/}
-                {/*~*/}
-            {/*</div>*/}
-        {/*</Jumbotron>*/}
-        {/*<div className="col-xs-1">*/}
-
-        {/*</div>*/}
-    {/*</div>*/}
-    {/*<div className="row">*/}
-        {/*<div className="col-xs-3">*/}
-        {/*</div>*/}
-        {/*<div>*/}
-            {/*<img className="landingPhoto col-xs-6" src={require("./images/landingPhotoV2.jpg")} alt="3 tier cake"/>*/}
-        {/*</div>*/}
-        {/*<div className="col-xs-4">*/}
-        {/*</div>*/}
-    {/*</div>*/}
-{/*</div>*/}
