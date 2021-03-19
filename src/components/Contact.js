5843f2ea2b56da4fafd3f525aa1ab871bb8a5eda
Nathan Wigen <nathan.s.wigen@hotmail.com>
2021-03-19 10:55:44 -0700

    working contact form.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,13 +2,22 @@ import React, { Component } from 'react';
 import { Jumbotron } from 'react-bootstrap';
 
 export default class Contact extends Component {
-    render() {
+  render() {
         return (
             <div className="row justify-content-center">
                 <Jumbotron className="Jumbotron-Global-Settings">
                     <div className="row justify-content-center">
-                        <h2 className="Contact-Header col-12">Get in touch to start planning the perfect cake for your occasion!</h2>
+                      <h2 className="Contact-Header col-12">Get in touch to start planning the perfect cake for your occasion!</h2>
                     </div>
+                      <div>
+                        <from>
+                          <label htmlFor="name" className="Contact-Name">Name:</label>
+                          <input name="Name" type="text" required placeholder="Your Name" className="Contact-Name-Input"/>
+                          <label htmlFor="email" className="Contact-Email">Email:</label>
+                          <input name="Email" type="text" required placeholder="Email" className="Contact-Email-Input"/>
+                          <button type="submit" className="Contact-Submit-Button">Get In Touch</button>
+                        </from>
+                      </div>
                 </Jumbotron>
             </div>
         );
